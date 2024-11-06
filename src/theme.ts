2171b62a2e73033c61a7bd178f2c33b1e57f87e3
Isidro Fernando Gonzalez Gomez <isidro.f.gonzalez@gmail.com>
2024-11-06 12:54:44 -0600

    feat: Implemented Material UI Theme provider with primary and secondary colors for the entire project.

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+// Configuración básica del tema
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#007FFF',
+    },
+    secondary: {
+      main: '#0056b3',
+    },
+  },
+});
+
+export default theme;
